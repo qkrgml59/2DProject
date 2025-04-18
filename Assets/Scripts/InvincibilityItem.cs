3354cdd09cfeefc64b994f9ae4ce6c002aca96fb
qkrgml59 <qkrgml59@gmail.com>
2025-04-19 03:57:36 +0900

    [Feat] 수정

---
+++ Assets/Scripts/InvincibilityItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvincibilityItem : MonoBehaviour
+{
+    public float invincibilityTime = 3f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+    }
+}
