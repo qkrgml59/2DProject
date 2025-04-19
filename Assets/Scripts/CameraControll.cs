cf72a39d6c71ec110cefeff850bfe7ee959de069
qkrgml59 <qkrgml59@gmail.com>
2025-04-20 05:56:12 +0900

    [Feat] 4라운드 맵 제작 & 아이템 스피드 무적 점프 제작 & 움직이는 적 제작

---
+++ Assets/Scripts/CameraControll.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    float cameraoffset = -10.0f;
+    public float FollowSpeed = 5.0f;
+    public float CameraPosY;
+
+    public float minX = -5.0f;
+    public float maxX = 10.0f;
+    public float minY = -5.0f;
+    public float maxY = 5.0f;
+
+    private void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null) return;
+
+        Vector3 targetPos = new Vector3(player.transform.position.x, player.transform.position.y + CameraPosY, cameraoffset);
+
+        // X 값 제한 적용
+        targetPos.x = Mathf.Clamp(targetPos.x, minX, maxX);
+
+        // Y 값 제한 적용
+        targetPos.y = Mathf.Clamp(targetPos.y, minY, maxY);
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, FollowSpeed * Time.deltaTime);
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Xml.Serialization;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using Unity.VisualScripting;
+using JetBrains.Annotations;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     public Transform groundCheck;
     public LayerMask groundLayer;
 
-    private Rigidbody2D rb;                        //점프의 첨 값을 준다
+    private Rigidbody2D rb;
 
-    public bool isGrounded = true;                         //땅에 있는지 체크 하는 변수 (true/false)
-
+    public bool isGrounded = true;
     public Animator myAnimator;
     public bool isJumping;
+
+    public bool invincibility;
+    public bool speedUP;
+    public bool jumpUP;
+    // === UI 관련 ===
+    public Image[] ItemUI;
+    
+   
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
+    private void Start()
+    {
+       
+    }
+
     private void Update()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
@@ -45,46 +56,105 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(1f, 1f, 1f);
             myAnimator.SetBool("Move", true);
         }
-      
-
-   
-
+        else
+        {
+            myAnimator.SetBool("Move", false);
+        }
     }
+
     void Jump()
     {
         isJumping = true;
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
+
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // "Ground" 태그를 가진 객체와 충돌 시
         if (collision.collider.CompareTag("Ground"))
         {
             isJumping = false;
         }
     }
+  
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Respawn"))
+       if (collision.CompareTag("Respawn"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-
-        if (collision.CompareTag("Finish"))
+        }    
+       
+       if (collision.CompareTag("Finish"))
         {
             collision.GetComponent<LevelObject>().MoveToNextLevel();
         }
 
-        if (collision.CompareTag("Enemy"))
+       if (collision.CompareTag("Enemy"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            if (invincibility)
+                Destroy(collision.gameObject);
+            else
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
 
+
+        if (collision.CompareTag("Item_1"))
+        {
+            invincibility = true;
+            Invoke("isNeverDie", 10f);
+            ItemUI[0].gameObject.SetActive(true);
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("Item_2"))
+        {
+            jumpUP = true;
+            Invoke("superjump", 3f);
+            ItemUI[1].gameObject.SetActive(true);
+            Destroy(collision.gameObject);
+        }
+        
+        if (collision.CompareTag("Item_3"))
+        {
+            speedUP = true;
+            if (speedUP == true)
+            {
+                moveSpeed = 10f;
+            }
+
+            Invoke("fast", 10f);
+            ItemUI[2].gameObject.SetActive(true);
+            Destroy(collision.gameObject);
+        }
+
+
     }
- 
+  
+    void isNeverDie()
+    {
+        invincibility = false;
+        ItemUI[0].gameObject.SetActive(false);
+    }
+     
+    void superjump()
+    {
+        jumpUP = false;
+        ItemUI[1].gameObject.SetActive(false);
+    }
+
+    void fast()
+    {
+        speedUP = false;
+        if (speedUP == false)
+        {
+            moveSpeed = 5f;
+        }
+        ItemUI[2].gameObject.SetActive(false);
+    }
+
 }
-
-
-    // Start is called before the first frame update
 
 
